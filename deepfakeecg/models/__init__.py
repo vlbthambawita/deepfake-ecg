6a5a97263403bc235c3ad69cb3117f4496468a07
Thomas Dreibholz <dreibh@simula.no>
2026-03-09 14:55:43 +0100

    Improved path settings.

--- deepfakeecg/models/__init__.py
+++ deepfakeecg/models/__init__.py
@@ -1,2 +1,2 @@
 from .pulse2pulse import Pulse2pulseDiscriminator as Discriminator
-from .pulse2pulse import Pulse2pulseGenerator as Generator
+from .pulse2pulse import Pulse2pulseGenerator     as Generator
